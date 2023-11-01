8098e08c4e8b2f4920d1a69da9657a6d28123145
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-31 22:30:03 -0500

    finished post model

---
+++ models/post.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_content: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+      }
+    );
+  
+    module.exports = Post;
